f773f94244a7fcba3f9a2a7fa4c7a6dbb416072b
Mrklata <58168326+Mrklata@users.noreply.github.com>
2020-08-08 18:39:32 +0200

    Delete test.py

--- test.py
+++
@@ -1,6 +0,0 @@
-import requests
-
-response = requests.get('https://www.googleapis.com/books/v1/volumes?q=Hobbit').json()
-data = response['items']
-
-print(data[0].get('volumeInfo', {}).get('title'))
